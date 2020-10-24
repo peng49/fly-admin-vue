afdfc6d1842ace47f9179483dee5f3d22a6d12c6
peng49 <1219955253@qq.com>
2020-10-24 11:38:18 +0800

    用户详情页面布局

--- src/views/user/view.vue
+++ src/views/user/view.vue
@@ -1,10 +1,45 @@
 <template>
-  <div />
+  <div class="app-container">
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-card class="box-card">
+          <div v-for="o in 4" :key="o" class="text item">
+            {{ '列表内容 ' + o }}
+          </div>
+        </el-card>
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>卡片名称</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <div v-for="o in 4" :key="o" class="text item">
+            {{ '列表内容 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="18">
+        <el-tabs v-model="activeName" type="border-card" @tab-click="handleClick">
+          <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+          <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+          <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+          <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+        </el-tabs>
+      </el-col>
+    </el-row>
+  </div>
+
 </template>
 <script>
 export default {
   data() {
-    return {}
+    return {
+      activeName: 'second'
+    }
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event)
+    }
   }
 }
 </script>
